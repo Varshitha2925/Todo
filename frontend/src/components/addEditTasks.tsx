c0aff3d542623981b7bb3019ac5ebbf8e527a43f
Varshitha2925 <varshithareddy2925@gmail.com>
2025-04-05 22:46:25 -0500

    change structure

---
+++ frontend/src/components/addEditTasks.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+interface Task {
+  _id: string;
+  text: string;
+  completed: boolean;
+}
+
+function addEditTasks() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState<string>("");
+
+  useEffect(() => {
+    axios.get<Task[]>("http://localhost:5000/tasks").then((res) => setTasks(res.data));
+  }, []);
+
+  const addTask = () => {
+    if (!newTask) return;
+    axios.post<Task>("http://localhost:5000/tasks", { text: newTask }).then((res) => {
+      setTasks([...tasks, res.data]);
+      setNewTask("");
+    });
+  };
+
+  const toggleTask = (id: string) => {
+    axios.put<Task>(`http://localhost:5000/tasks/${id}`).then((res) => {
+      setTasks(tasks.map((task) => (task._id === id ? res.data : task)));
+    });
+  };
+
+  const deleteTask = (id: string) => {
+    axios.delete(`http://localhost:5000/tasks/${id}`).then(() => {
+      setTasks(tasks.filter((task) => task._id !== id));
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Add a task..."
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task._id} style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+            {task.text}
+            <button onClick={() => toggleTask(task._id)}>✔</button>
+            <button onClick={() => deleteTask(task._id)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default addEditTasks;
