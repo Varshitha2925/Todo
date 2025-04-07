0cac9265a0c637d68a12d05c6990f089c8d599bb
Varshitha2925 <varshithareddy2925@gmail.com>
2025-04-06 22:59:57 -0500

    strcture change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,60 +1,12 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import "./App.css";
+import AddEditTasks from "./components/addEditTasks";
 
-interface Task {
-  _id: string;
-  text: string;
-  completed: boolean;
-}
+import React from "react";
 
-function App() {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTask, setNewTask] = useState<string>("");
-
-  useEffect(() => {
-    axios.get<Task[]>("http://localhost:5000/tasks").then((res) => setTasks(res.data));
-  }, []);
-
-  const addTask = () => {
-    if (!newTask) return;
-    axios.post<Task>("http://localhost:5000/tasks", { text: newTask }).then((res) => {
-      setTasks([...tasks, res.data]);
-      setNewTask("");
-    });
-  };
-
-  const toggleTask = (id: string) => {
-    axios.put<Task>(`http://localhost:5000/tasks/${id}`).then((res) => {
-      setTasks(tasks.map((task) => (task._id === id ? res.data : task)));
-    });
-  };
-
-  const deleteTask = (id: string) => {
-    axios.delete(`http://localhost:5000/tasks/${id}`).then(() => {
-      setTasks(tasks.filter((task) => task._id !== id));
-    });
-  };
-
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>To-Do List</h1>
-      <input
-        type="text"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-        placeholder="Add a task..."
-      />
-      <button onClick={addTask}>Add</button>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task._id} style={{ textDecoration: task.completed ? "line-through" : "none" }}>
-            {task.text}
-            <button onClick={() => toggleTask(task._id)}>✔</button>
-            <button onClick={() => deleteTask(task._id)}>❌</button>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <AddEditTasks />
     </div>
   );
 }
