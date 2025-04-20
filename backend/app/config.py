de193096cc912b56a487811ff30d39d5e2408e09
Varshitha2925 <varshithareddy2925@gmail.com>
2025-04-20 15:56:27 -0500

    updated fronted

--- backend/app/config.py
+++
@@ -1,2 +0,0 @@
-class Config:
-    MONGO_URI = "mongodb+srv://varshithareddy2925:vinny@cluster0.cnqnwrz.mongodb.net/Todo?retryWrites=true&w=majority"
--- backend/app/models.py
+++
@@ -1,8 +0,0 @@
-def task_to_json(task):
-    return {
-        "_id": str(task["_id"]),
-        "text": task["text"],
-        "completed": task["completed"],
-        "dueDate": task.get("dueDate"),
-        "priority": task.get("priority", "Low")
-    }
